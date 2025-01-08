85604b5f5f1b0e80e0bd4d0549ca03a476b3e065
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2025-01-08 14:16:46 -0800

    `swap force` command

--- src/main/java/com/zenith/command/impl/SpectatorSwapCommand.java
+++ src/main/java/com/zenith/command/impl/SpectatorSwapCommand.java
@@ -9,79 +9,108 @@ import com.zenith.command.CommandUsage;
 import com.zenith.command.brigadier.CommandCategory;
 import com.zenith.command.brigadier.CommandContext;
 import com.zenith.command.brigadier.CommandSource;
+import com.zenith.network.server.ServerSession;
 import com.zenith.util.ComponentSerializer;
 import org.geysermc.mcprotocollib.protocol.packet.ingame.clientbound.ClientboundSystemChatPacket;
 
 import java.util.Optional;
 
-import static com.zenith.Shared.CONFIG;
-import static com.zenith.Shared.PLAYER_LISTS;
+import static com.zenith.Shared.*;
 import static java.util.Arrays.asList;
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 public class SpectatorSwapCommand extends Command {
     @Override
     public CommandUsage commandUsage() {
-        return CommandUsage.simple(
+        return CommandUsage.args(
             "swap",
             CommandCategory.MODULE,
             """
             Swaps the current controlling player to spectator mode.
-            """
+            """,
+            asList(
+                "",
+                "force"
+            )
         );
     }
 
     @Override
     public LiteralArgumentBuilder<CommandContext> register() {
-        return command("swap").requires(c -> Command.validateCommandSource(c, asList(CommandSource.IN_GAME_PLAYER, CommandSource.SPECTATOR))).executes(c -> {
-            if (c.getSource().getSource() == CommandSource.IN_GAME_PLAYER) {
-                var player = Proxy.getInstance().getActivePlayer();
-                if (player == null) {
-                    c.getSource().getEmbed()
-                        .title("Unable to Swap")
-                        .errorColor()
-                        .description("No player is currently controlling the proxy account");
-                    return;
-                }
-                if (CONFIG.server.viaversion.enabled) {
-                    Optional<ProtocolVersion> viaClientProtocolVersion = Via.getManager().getConnectionManager().getConnectedClients().values().stream()
-                        .filter(client -> client.getChannel() == player.getChannel())
-                        .map(con -> con.getProtocolInfo().protocolVersion())
-                        .findFirst();
-                    if (viaClientProtocolVersion.isPresent() && viaClientProtocolVersion.get().olderThan(ProtocolVersion.v1_20_5)) {
-                        c.getSource().getEmbed()
-                            .title("Unsupported Client MC Version")
-                            .errorColor()
-                            .addField("Client Version", viaClientProtocolVersion.get().getName(), false)
-                            .addField("Error", "Client version must be at least 1.20.6", false);
-                        return;
-                    }
-                }
-                player.transferToSpectator(CONFIG.server.getProxyAddressForTransfer(), CONFIG.server.getProxyPortForTransfer());
-            } else if (c.getSource().getSource() == CommandSource.SPECTATOR) {
-                var session = c.getSource().getInGamePlayerInfo().session();
-                var spectatorProfile = session.getProfileCache().getProfile();
-                c.getSource().setNoOutput(true);
-                if (spectatorProfile == null) return;
-                if (!PLAYER_LISTS.getWhitelist().contains(spectatorProfile.getId())) {
-                    session.send(new ClientboundSystemChatPacket(ComponentSerializer.minimessage("<red>You are not whitelisted!"), false));
-                    return;
-                }
-                if (Proxy.getInstance().getActivePlayer() != null) {
-                    session.send(new ClientboundSystemChatPacket(ComponentSerializer.minimessage("<red>Someone is already controlling the player!"), false));
-                    return;
-                }
-                if (CONFIG.server.viaversion.enabled) {
-                    Optional<ProtocolVersion> viaClientProtocolVersion = Via.getManager().getConnectionManager().getConnectedClients().values().stream()
-                        .filter(client -> client.getChannel() == session.getChannel())
-                        .map(con -> con.getProtocolInfo().protocolVersion())
-                        .findFirst();
-                    if (viaClientProtocolVersion.isPresent() && viaClientProtocolVersion.get().olderThan(ProtocolVersion.v1_20_5)) {
-                        session.send(new ClientboundSystemChatPacket(ComponentSerializer.minimessage("<red>Unsupported Client MC Version"), false));
-                        return;
-                    }
-                }
-                session.transferToControllingPlayer(CONFIG.server.getProxyAddressForTransfer(), CONFIG.server.getProxyPortForTransfer());
+        return command("swap").requires(c -> Command.validateCommandSource(c, asList(CommandSource.IN_GAME_PLAYER, CommandSource.SPECTATOR)))
+            .executes(c -> {
+                swap(c, false);
+            })
+            .then(literal("force").requires(Command::validateAccountOwner).executes(c -> {
+                swap(c, true);
+            }));
+    }
+
+    private void swap(com.mojang.brigadier.context.CommandContext<CommandContext> c, boolean force) {
+        ServerSession activePlayer = Proxy.getInstance().getActivePlayer();
+        if (c.getSource().getSource() == CommandSource.IN_GAME_PLAYER) {
+            var player = activePlayer;
+            if (player == null) {
+                c.getSource().getEmbed()
+                    .title("Unable to Swap")
+                    .errorColor()
+                    .description("No player is currently controlling the proxy account");
+                return;
             }
-        });
+            if (CONFIG.server.viaversion.enabled) {
+                Optional<ProtocolVersion> viaClientProtocolVersion = Via.getManager().getConnectionManager().getConnectedClients().values().stream()
+                    .filter(client -> client.getChannel() == player.getChannel())
+                    .map(con -> con.getProtocolInfo().protocolVersion())
+                    .findFirst();
+                if (viaClientProtocolVersion.isPresent() && viaClientProtocolVersion.get().olderThan(ProtocolVersion.v1_20_5)) {
+                    c.getSource().getEmbed()
+                        .title("Unsupported Client MC Version")
+                        .errorColor()
+                        .addField("Client Version", viaClientProtocolVersion.get().getName(), false)
+                        .addField("Error", "Client version must be at least 1.20.6", false);
+                    return;
+                }
+            }
+            player.transferToSpectator(CONFIG.server.getProxyAddressForTransfer(), CONFIG.server.getProxyPortForTransfer());
+        } else if (c.getSource().getSource() == CommandSource.SPECTATOR) {
+            var session = c.getSource().getInGamePlayerInfo().session();
+            var spectatorProfile = session.getProfileCache().getProfile();
+            c.getSource().setNoOutput(true);
+            if (spectatorProfile == null) return;
+            if (!PLAYER_LISTS.getWhitelist().contains(spectatorProfile.getId())) {
+                session.send(new ClientboundSystemChatPacket(ComponentSerializer.minimessage("<red>You are not whitelisted!"), false));
+                return;
+            }
+            if (CONFIG.server.viaversion.enabled) {
+                Optional<ProtocolVersion> viaClientProtocolVersion = Via.getManager().getConnectionManager().getConnectedClients().values().stream()
+                    .filter(client -> client.getChannel() == session.getChannel())
+                    .map(con -> con.getProtocolInfo().protocolVersion())
+                    .findFirst();
+                if (viaClientProtocolVersion.isPresent() && viaClientProtocolVersion.get().olderThan(ProtocolVersion.v1_20_5)) {
+                    session.send(new ClientboundSystemChatPacket(ComponentSerializer.minimessage("<red>Unsupported Client MC Version"), false));
+                    return;
+                }
+            }
+            if (activePlayer != null) {
+                if (force) {
+                    if (CONFIG.server.viaversion.enabled) {
+                        Optional<ProtocolVersion> viaClientControllerProtocolVersion = Via.getManager().getConnectionManager().getConnectedClients().values().stream()
+                            .filter(client -> client.getChannel() == activePlayer.getChannel())
+                            .map(con -> con.getProtocolInfo().protocolVersion())
+                            .findFirst();
+                        if (viaClientControllerProtocolVersion.isPresent() && viaClientControllerProtocolVersion.get().olderThan(ProtocolVersion.v1_20_5)) {
+                            session.send(new ClientboundSystemChatPacket(ComponentSerializer.minimessage("<red>Controlling player is using an unsupported Client MC Version"), false));
+                            return;
+                        }
+                    }
+                    activePlayer.transferToSpectator(CONFIG.server.getProxyAddressForTransfer(), CONFIG.server.getProxyPortForTransfer());
+                    EXECUTOR.schedule(() -> session.transferToControllingPlayer(CONFIG.server.getProxyAddressForTransfer(), CONFIG.server.getProxyPortForTransfer()), 1, SECONDS);
+                } else {
+                    session.send(new ClientboundSystemChatPacket(ComponentSerializer.minimessage("<red>Someone is already controlling the player!"), false));
+                }
+                return;
+            }
+            session.transferToControllingPlayer(CONFIG.server.getProxyAddressForTransfer(), CONFIG.server.getProxyPortForTransfer());
+        }
     }
 }
