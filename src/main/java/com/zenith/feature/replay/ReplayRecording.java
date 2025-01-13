1cb1ee1157b6c7e69bb423615df062aead03f113
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2025-01-12 16:28:36 -0800

    synchronize more replay methods

--- src/main/java/com/zenith/feature/replay/ReplayRecording.java
+++ src/main/java/com/zenith/feature/replay/ReplayRecording.java
@@ -60,7 +60,7 @@ public class ReplayRecording implements Closeable {
         this.replayDirectory = replayDirectory;
     }
 
-    public void startRecording() throws Exception{
+    public synchronized void startRecording() throws Exception {
         // initialize output streams and metadata
         var serverName = CONFIG.client.server.address;
         if (CONFIG.client.server.port != 25565)
@@ -87,7 +87,7 @@ public class ReplayRecording implements Closeable {
     }
 
     // Start recording while we already have a logged in session
-    private void lateStartRecording() {
+    private synchronized void lateStartRecording() {
         writePacket0(0, new ClientboundGameProfilePacket(CACHE.getProfileCache().getProfile(), false), Proxy.getInstance().getClient(), ProtocolState.LOGIN);
         CACHE.getConfigurationCache().getPackets(
             packet -> writePacket0(System.currentTimeMillis(), (MinecraftPacket) packet, Proxy.getInstance().getClient(), ProtocolState.CONFIGURATION),
@@ -142,7 +142,7 @@ public class ReplayRecording implements Closeable {
         if (!executor.isShutdown()) executor.execute(() -> writePacket0(time, packet, session, protocolState));
     }
 
-    private void writePacket0(final long time, final MinecraftPacket packet, final Session session, final ProtocolState protocolState) {
+    private synchronized void writePacket0(final long time, final MinecraftPacket packet, final Session session, final ProtocolState protocolState) {
         try {
             writeToFile(time, packet, session, protocolState);
         } catch (final Throwable e) {
@@ -150,7 +150,7 @@ public class ReplayRecording implements Closeable {
         }
     }
 
-    private void writeToFile(final long time, final MinecraftPacket packet, final Session session, final ProtocolState protocolState) {
+    private synchronized void writeToFile(final long time, final MinecraftPacket packet, final Session session, final ProtocolState protocolState) {
         int t = time == 0 ? 0 : (int) (time - startT);
         if (t == 0) startT = System.currentTimeMillis();
         final ByteBuf packetBuf = ALLOC.heapBuffer();
@@ -175,7 +175,7 @@ public class ReplayRecording implements Closeable {
     }
 
     @Override
-    public void close() throws IOException {
+    public synchronized void close() throws IOException {
         if (!executor.isShutdown()) {
             try {
                 executor.shutdown();
