b65343d3c2ae54245162afb99f35c812560d43ca
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2025-02-02 14:38:15 -0800

    handle irregular inputs in server command

--- src/main/java/com/zenith/command/impl/ServerCommand.java
+++ src/main/java/com/zenith/command/impl/ServerCommand.java
@@ -1,5 +1,6 @@
 package com.zenith.command.impl;
 
+import com.google.common.primitives.Ints;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
@@ -9,12 +10,17 @@ import com.zenith.command.brigadier.CommandCategory;
 import com.zenith.command.brigadier.CommandContext;
 import com.zenith.discord.Embed;
 
+import java.util.regex.Pattern;
+
 import static com.mojang.brigadier.arguments.IntegerArgumentType.integer;
 import static com.zenith.Shared.CONFIG;
 import static com.zenith.command.brigadier.CustomStringArgumentType.wordWithChars;
 import static java.util.Arrays.asList;
 
 public class ServerCommand extends Command {
+    private final Pattern ipWithPortPattern = Pattern.compile("^(?:[0-9]{1,3}\\.){3}[0-9]{1,3}:[0-9]{1,5}$");
+    private final Pattern domainWithPortPattern = Pattern.compile("^[a-zA-Z0-9]+:[0-9]{1,5}$");
+    private final Pattern ipv6Pattern = Pattern.compile("^((([0-9A-Fa-f]{1,4}:){1,6}:)|(([0-9A-Fa-f]{1,4}:){7}))([0-9A-Fa-f]{1,4})$");
     @Override
     public CommandUsage commandUsage() {
         return CommandUsage.args(
@@ -43,11 +49,60 @@ public class ServerCommand extends Command {
                       }))
                       .executes(c -> {
                           final String ip = StringArgumentType.getString(c, "ip");
-                          CONFIG.client.server.address = (ip.equalsIgnoreCase("2b2t") ? "connect.2b2t.org" : ip);
-                          CONFIG.client.server.port = 25565;
-                          c.getSource().getEmbed()
-                              .title("Server Updated!");
-                          return OK;
+                          if (ipWithPortPattern.matcher(ip).matches()) {
+                              final String[] split = ip.split(":");
+                              if (split.length != 2) {
+                                  c.getSource().getEmbed()
+                                      .title("Error")
+                                      .description("Invalid IP format.");
+                                  return OK;
+                              }
+                              String ipExtracted = split[0];
+                              Integer p = Ints.tryParse(split[1]);
+                              if (p == null) {
+                                  c.getSource().getEmbed()
+                                      .title("Error")
+                                      .description("Invalid IP format.");
+                                  return OK;
+                              }
+                              CONFIG.client.server.address = ipExtracted;
+                              CONFIG.client.server.port = p;
+                                c.getSource().getEmbed()
+                                    .title("Server Updated!");
+                              return OK;
+                          } else if (domainWithPortPattern.matcher(ip).matches()) {
+                              final String[] split = ip.split(":");
+                              if (split.length != 2) {
+                                  c.getSource().getEmbed()
+                                      .title("Error")
+                                      .description("Invalid IP format.");
+                                  return OK;
+                              }
+                              String ipExtracted = split[0];
+                              Integer p = Ints.tryParse(split[1]);
+                              if (p == null) {
+                                  c.getSource().getEmbed()
+                                      .title("Error")
+                                      .description("Invalid IP format.");
+                                  return OK;
+                              }
+                              CONFIG.client.server.address = ipExtracted;
+                              CONFIG.client.server.port = p;
+                              c.getSource().getEmbed()
+                                  .title("Server Updated!");
+                              return OK;
+                          } else if (ipv6Pattern.matcher(ip).matches()) {
+                              CONFIG.client.server.address = ip;
+                              CONFIG.client.server.port = 25565;
+                              c.getSource().getEmbed()
+                                  .title("Server Updated!");
+                              return OK;
+                          } else {
+                              c.getSource().getEmbed()
+                                  .title("Error")
+                                  .description("Invalid IP format.");
+                              return OK;
+                          }
                       }));
     }
 
