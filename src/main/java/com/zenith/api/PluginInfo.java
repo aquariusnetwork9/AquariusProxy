95e2a74df706db9b31a57ca458977a540e0afd7e
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2025-03-04 21:12:25 -0800

    comment

--- src/main/java/com/zenith/api/PluginInfo.java
+++ src/main/java/com/zenith/api/PluginInfo.java
@@ -2,6 +2,9 @@ package com.zenith.api;
 
 import java.util.List;
 
+/**
+ * Data from each plugin's {@link Plugin} annotation.
+ */
 public record PluginInfo(
     String entrypoint,
     String id,
