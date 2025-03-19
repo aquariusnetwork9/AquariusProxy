511f1318e3b9f117203e1399406b799896d43389
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2025-03-18 22:23:00 -0700

    prevent queue and world switches from leaking the offset

--- src/main/java/com/zenith/feature/coordobf/handlers/outbound/COLoginHandler.java
+++ src/main/java/com/zenith/feature/coordobf/handlers/outbound/COLoginHandler.java
@@ -3,22 +3,27 @@ package com.zenith.feature.coordobf.handlers.outbound;
 import com.zenith.Proxy;
 import com.zenith.network.registry.PacketHandler;
 import com.zenith.network.server.ServerSession;
-import com.zenith.util.Config.Client.Extra.CoordObfuscation.ObfuscationMode;
+import org.geysermc.mcprotocollib.protocol.data.game.entity.player.GameMode;
 import org.geysermc.mcprotocollib.protocol.data.game.entity.player.PlayerSpawnInfo;
 import org.geysermc.mcprotocollib.protocol.packet.ingame.clientbound.ClientboundLoginPacket;
-
-import static com.zenith.Shared.CONFIG;
 
 public class COLoginHandler implements PacketHandler<ClientboundLoginPacket, ServerSession> {
     @Override
     public ClientboundLoginPacket apply(final ClientboundLoginPacket packet, final ServerSession session) {
-        if (CONFIG.client.extra.coordObfuscation.mode == ObfuscationMode.CONSTANT_OFFSET) {
-            if (!Proxy.getInstance().getClient().isOnline()) {
-                // prevent queue from leaking a constant offset
+        if (session.isInGame()) {
+            // i.e. velocity world switching
+            session.disconnect("World switching");
+            return null;
+        }
+        if (Proxy.getInstance().isOn2b2t())
+            if (!Proxy.getInstance().getClient().isOnline()
+                || Proxy.getInstance().isInQueue()
+                || packet.getCommonPlayerSpawnInfo().getGameMode() == GameMode.SPECTATOR
+            ) {
+                // prevent queue from leaking the offset
                 session.disconnect("Queueing");
                 return null;
             }
-        }
         return new ClientboundLoginPacket(
             packet.getEntityId(),
             packet.isHardcore(),
