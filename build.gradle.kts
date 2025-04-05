fc99e69c7fbd68a77512d0139ac43390993bad44
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2025-04-04 22:32:01 -0700

    update mcpl

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,7 +55,7 @@ dependencies {
         exclude(group = "net.java.dev.jna")
         exclude(group = "com.google.crypto.tink")
     }
-    api("com.github.rfresh2:MCProtocolLib:1.21.0.37") {
+    api("com.github.rfresh2:MCProtocolLib:1.21.0.38") {
         exclude(group = "io.netty")
     }
     val nettyVersion = "4.1.119.Final"
