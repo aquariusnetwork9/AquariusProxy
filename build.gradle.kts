5fb8e5de2f9910ea16cc67365830c757ce074936
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2025-04-05 16:30:29 -0700

    adventure 4.20.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,7 +55,7 @@ dependencies {
         exclude(group = "net.java.dev.jna")
         exclude(group = "com.google.crypto.tink")
     }
-    api("com.github.rfresh2:MCProtocolLib:1.21.0.40") {
+    api("com.github.rfresh2:MCProtocolLib:1.21.0.41") {
         exclude(group = "io.netty")
     }
     val nettyVersion = "4.2.0.Final"
