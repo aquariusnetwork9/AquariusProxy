1100e3f0cfade49365dee7aa33409892d2d0a10f
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2025-04-06 21:06:40 -0700

    base class for module util methods

---
+++ src/main/java/com/zenith/module/ModuleUtils.java
@@ -0,0 +1,108 @@
+package com.zenith.module;
+
+import com.zenith.Proxy;
+import com.zenith.command.util.CommandOutputHelper;
+import com.zenith.discord.Embed;
+import com.zenith.network.client.ClientSession;
+import org.geysermc.mcprotocollib.network.packet.Packet;
+
+import static com.zenith.Globals.DISCORD;
+import static com.zenith.Globals.MODULE_LOG;
+
+public abstract class ModuleUtils {
+    public void sendClientPacketAsync(final Packet packet) {
+        ClientSession clientSession = Proxy.getInstance().getClient();
+        if (clientSession != null && clientSession.isConnected()) {
+            clientSession.sendAsync(packet);
+        }
+    }
+
+    public void sendClientPacket(final Packet packet) {
+        ClientSession clientSession = Proxy.getInstance().getClient();
+        if (clientSession != null && clientSession.isConnected()) {
+            clientSession.send(packet);
+        }
+    }
+
+    public void sendClientPacketAwait(final Packet packet) {
+        ClientSession clientSession = Proxy.getInstance().getClient();
+        if (clientSession != null && clientSession.isConnected()) {
+            try {
+                clientSession.sendAwait(packet);
+            } catch (Exception e) {
+                error("Error sending awaited packet: {}", packet.getClass().getSimpleName(), e);
+            }
+        }
+    }
+
+    // preserves packet order
+    public void sendClientPacketsAsync(final Packet... packets) {
+        ClientSession clientSession = Proxy.getInstance().getClient();
+        if (clientSession != null && clientSession.isConnected()) {
+            for (Packet packet : packets) {
+                clientSession.sendAsync(packet);
+            }
+        }
+    }
+
+    protected final String moduleLogPrefix = "[" + this.getClass().getSimpleName() + "] ";
+
+    public void info(String msg) {
+        MODULE_LOG.info(moduleLogPrefix + msg);
+    }
+
+    public void info(String msg, Object... args) {
+        MODULE_LOG.info(moduleLogPrefix + msg, args);
+    }
+
+    public void error(String msg) {
+        MODULE_LOG.error(moduleLogPrefix + msg);
+    }
+
+    public void error(String msg, Object... args) {
+        MODULE_LOG.error(moduleLogPrefix + msg, args);
+    }
+
+    public void debug(String msg) {
+        MODULE_LOG.debug(moduleLogPrefix + msg);
+    }
+
+    public void debug(String msg, Object... args) {
+        MODULE_LOG.debug(moduleLogPrefix + msg, args);
+    }
+
+    public void warn(String msg) {
+        MODULE_LOG.warn(moduleLogPrefix + msg);
+    }
+
+    public void warn(String msg, Object... args) {
+        MODULE_LOG.warn(moduleLogPrefix + msg, args);
+    }
+
+    protected final String moduleAlertPrefix = "<gray>[<aqua>" + this.getClass().getSimpleName() + "<gray>]<reset> ";
+
+    public void inGameAlert(String minedown) {
+        var connections = Proxy.getInstance().getActiveConnections().getArray();
+        for (int i = 0; i < connections.length; i++) {
+            var connection = connections[i];
+            connection.sendAsyncAlert(moduleAlertPrefix + minedown);
+        }
+    }
+
+    public void inGameAlertActivePlayer(String minedown) {
+        var connection = Proxy.getInstance().getActivePlayer();
+        if (connection == null) return;
+        connection.sendAsyncAlert(moduleAlertPrefix + minedown);
+    }
+
+    // is also logged to the terminal
+    public void discordNotification(Embed embed) {
+        embed.title("[" + this.getClass().getSimpleName() + "] " + (embed.isTitlePresent() ? embed.title() : ""));
+        DISCORD.sendEmbedMessage(embed);
+    }
+
+    public void discordAndIngameNotification(Embed embed) {
+        discordNotification(embed);
+        CommandOutputHelper.logEmbedOutputToInGameAllConnectedPlayers(embed);
+    }
+}
