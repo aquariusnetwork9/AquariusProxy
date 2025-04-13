3c4197719591b062d479300ba0f2a700966db3fa
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2025-04-13 13:43:59 -0700

    `actionLimiter allowServerCommands` cancels command suggestions

--- src/main/java/com/zenith/module/impl/ActionLimiter.java
+++ src/main/java/com/zenith/module/impl/ActionLimiter.java
@@ -5,6 +5,7 @@ import com.zenith.event.player.PlayerConnectionRemovedEvent;
 import com.zenith.event.player.PlayerLoginEvent;
 import com.zenith.feature.actionlimiter.handlers.inbound.*;
 import com.zenith.feature.actionlimiter.handlers.outbound.ALCMoveVehicleHandler;
+import com.zenith.feature.actionlimiter.handlers.outbound.ALCommandSuggestionsHandler;
 import com.zenith.feature.actionlimiter.handlers.outbound.ALLoginHandler;
 import com.zenith.feature.actionlimiter.handlers.outbound.ALPlayerPositionHandler;
 import com.zenith.module.api.Module;
@@ -15,13 +16,11 @@ import com.zenith.util.math.MathHelper;
 import it.unimi.dsi.fastutil.objects.ReferenceOpenHashSet;
 import it.unimi.dsi.fastutil.objects.ReferenceSet;
 import org.geysermc.mcprotocollib.protocol.data.ProtocolState;
+import org.geysermc.mcprotocollib.protocol.packet.ingame.clientbound.ClientboundCommandSuggestionsPacket;
 import org.geysermc.mcprotocollib.protocol.packet.ingame.clientbound.ClientboundLoginPacket;
 import org.geysermc.mcprotocollib.protocol.packet.ingame.clientbound.entity.ClientboundMoveVehiclePacket;
 import org.geysermc.mcprotocollib.protocol.packet.ingame.clientbound.entity.player.ClientboundPlayerPositionPacket;
-import org.geysermc.mcprotocollib.protocol.packet.ingame.serverbound.ServerboundChatCommandPacket;
-import org.geysermc.mcprotocollib.protocol.packet.ingame.serverbound.ServerboundChatCommandSignedPacket;
-import org.geysermc.mcprotocollib.protocol.packet.ingame.serverbound.ServerboundChatPacket;
-import org.geysermc.mcprotocollib.protocol.packet.ingame.serverbound.ServerboundClientCommandPacket;
+import org.geysermc.mcprotocollib.protocol.packet.ingame.serverbound.*;
 import org.geysermc.mcprotocollib.protocol.packet.ingame.serverbound.inventory.ServerboundContainerClickPacket;
 import org.geysermc.mcprotocollib.protocol.packet.ingame.serverbound.inventory.ServerboundEditBookPacket;
 import org.geysermc.mcprotocollib.protocol.packet.ingame.serverbound.level.ServerboundMoveVehiclePacket;
@@ -47,6 +46,7 @@ public class ActionLimiter extends Module {
                 .inbound(ServerboundChatCommandSignedPacket.class, new ALSignedChatCommandHandler())
                 .inbound(ServerboundChatPacket.class, new ALChatHandler())
                 .inbound(ServerboundClientCommandPacket.class, new ALClientCommandHandler())
+                .inbound(ServerboundCommandSuggestionPacket.class, new ALCommandSuggestionHandler())
                 .inbound(ServerboundContainerClickPacket.class, new ALContainerClickHandler())
                 .inbound(ServerboundInteractPacket.class, new ALInteractHandler())
                 .inbound(ServerboundMovePlayerPosPacket.class, new ALMovePlayerPosHandler())
@@ -57,6 +57,7 @@ public class ActionLimiter extends Module {
                 .inbound(ServerboundUseItemPacket.class, new ALUseItemHandler())
                 .inbound(ServerboundEditBookPacket.class, new ALEditBookHandler())
                 .outbound(ClientboundMoveVehiclePacket.class, new ALCMoveVehicleHandler())
+                .outbound(ClientboundCommandSuggestionsPacket.class, new ALCommandSuggestionsHandler())
                 .outbound(ClientboundLoginPacket.class, new ALLoginHandler())
                 .outbound(ClientboundPlayerPositionPacket.class, new ALPlayerPositionHandler())
                 .build())
