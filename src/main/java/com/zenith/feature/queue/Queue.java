70de2f75bb94c6cb08dc44115cf1898f0869d62b
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2025-04-13 15:34:05 -0700

    update default eta equation

--- src/main/java/com/zenith/feature/queue/Queue.java
+++ src/main/java/com/zenith/feature/queue/Queue.java
@@ -21,7 +21,7 @@ public class Queue {
     private static QueueStatus queueStatus = new QueueStatus(0, 0, 0);
     private static final Pattern digitPattern = Pattern.compile("\\d+");
     private volatile static Instant lastUpdate = Instant.EPOCH;
-    private static QueueEtaEquationResponse queueEtaEquation = new QueueEtaEquationResponse(343.0, 0.743);
+    private static QueueEtaEquationResponse queueEtaEquation = new QueueEtaEquationResponse(199.0, 0.758);
     private static Instant lastQueueEtaEquationUpdate = Instant.EPOCH;
 
     public static void start() {
