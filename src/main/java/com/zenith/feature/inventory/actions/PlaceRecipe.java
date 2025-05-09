c63506df246beb73797eb00a9bb8b5bc8858a867
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2025-05-09 02:36:20 -0700

    use api status annotation

--- src/main/java/com/zenith/feature/inventory/actions/PlaceRecipe.java
+++ src/main/java/com/zenith/feature/inventory/actions/PlaceRecipe.java
@@ -3,15 +3,19 @@ package com.zenith.feature.inventory.actions;
 import lombok.Data;
 import org.geysermc.mcprotocollib.protocol.codec.MinecraftPacket;
 import org.geysermc.mcprotocollib.protocol.packet.ingame.serverbound.inventory.ServerboundPlaceRecipePacket;
+import org.jetbrains.annotations.ApiStatus;
 import org.jspecify.annotations.Nullable;
 
+/**
+ * Warning: unstable interface
+ * MC 1.21.4+ uses integer recipe id's instead of identifiers
+ *
+ * To find recipes, query `Globals.CACHE.getRecipeCache().getRecipeRegistry()`
+ */
 @Data
+@ApiStatus.Experimental
 public class PlaceRecipe implements InventoryAction {
     private final int containerId;
-    /**
-     * Warning: unstable interface
-     * MC 1.21.4 uses integer recipe id's instead of identifiers
-     */
     private final String recipeId;
     private final boolean useMaxItems;
 
