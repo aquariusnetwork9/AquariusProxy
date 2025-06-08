38ae457c4dc6fb1766e36ffcce4587c3ab0d0758
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2025-06-07 20:28:57 -0700

    implement PlaceRecipe item key api like 1.21.0

--- src/main/java/com/zenith/feature/inventory/actions/PlaceRecipe.java
+++ src/main/java/com/zenith/feature/inventory/actions/PlaceRecipe.java
@@ -1,22 +1,28 @@
 package com.zenith.feature.inventory.actions;
 
+import com.zenith.mc.item.ItemData;
+import com.zenith.mc.item.ItemRegistry;
 import lombok.Data;
+import net.kyori.adventure.key.Key;
 import org.geysermc.mcprotocollib.protocol.codec.MinecraftPacket;
+import org.geysermc.mcprotocollib.protocol.data.game.item.ItemStack;
+import org.geysermc.mcprotocollib.protocol.data.game.recipe.display.slot.ItemStackSlotDisplay;
 import org.geysermc.mcprotocollib.protocol.packet.ingame.serverbound.inventory.ServerboundPlaceRecipePacket;
 import org.jetbrains.annotations.ApiStatus;
 import org.jspecify.annotations.Nullable;
 
+import static com.zenith.Globals.CACHE;
+import static com.zenith.Globals.CLIENT_LOG;
+
 /**
  * Warning: unstable interface
- * MC 1.21.4+ uses integer recipe id's instead of identifiers
- *
- * To find recipes, query `Globals.CACHE.getRecipeCache().getRecipeRegistry()`
+ * May change in future MC versions
  */
 @Data
 @ApiStatus.Experimental
 public class PlaceRecipe implements InventoryAction {
     private final int containerId;
-    private final int recipeId;
+    private final String recipeId;
     private final boolean useMaxItems;
 
     @Override
@@ -26,6 +32,28 @@ public class PlaceRecipe implements InventoryAction {
 
     @Override
     public @Nullable MinecraftPacket packet() {
-        return new ServerboundPlaceRecipePacket(containerId, recipeId, useMaxItems);
+        // chop off the namespace prefix if it exists (like "minecraft:emerald_block" -> "emerald_block")
+        String recipeKey;
+        try {
+            recipeKey = Key.key(recipeId).value();
+        } catch (Exception e) {
+            CLIENT_LOG.debug("Invalid recipe key: {}", recipeId, e);
+            return null;
+        }
+        ItemData itemData = ItemRegistry.REGISTRY.get(recipeKey);
+        if (itemData == null) {
+            CLIENT_LOG.debug("No item data found for recipe {}", this);
+            return null;
+        }
+        for (var recipeBookEntry : CACHE.getRecipeCache().getRecipeBookEntries().int2ObjectEntrySet()) {
+            var displayResult = recipeBookEntry.getValue().display().result();
+            if (displayResult instanceof ItemStackSlotDisplay(ItemStack itemStack)) {
+                if (itemStack != null && itemStack.getId() == itemData.id()) {
+                    return new ServerboundPlaceRecipePacket(containerId, recipeBookEntry.getIntKey(), useMaxItems);
+                }
+            }
+        }
+        CLIENT_LOG.debug("No matching recipe found {}", this);
+        return null;
     }
 }
