a9fb90d5fe6dd574f781dbe7eec57ce8285da06e
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2025-08-11 01:24:09 -0700

    lazy init blockoptionalmeta lists

--- src/main/java/com/zenith/feature/pathfinder/util/BlockOptionalMetaLookup.java
+++ src/main/java/com/zenith/feature/pathfinder/util/BlockOptionalMetaLookup.java
@@ -2,14 +2,17 @@ package com.zenith.feature.pathfinder.util;
 
 import com.google.common.collect.Sets;
 import com.zenith.mc.block.Block;
+import com.zenith.mc.block.BlockRegistry;
 import com.zenith.mc.block.BlockState;
 import it.unimi.dsi.fastutil.ints.IntArrayList;
 import it.unimi.dsi.fastutil.ints.IntList;
 import it.unimi.dsi.fastutil.ints.IntOpenHashSet;
 import it.unimi.dsi.fastutil.ints.IntSet;
 import lombok.Data;
+import lombok.Getter;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -17,23 +20,38 @@ import java.util.Set;
 public class BlockOptionalMetaLookup {
     private final Set<Block> blockSet;
     // sets preferable for fast contains
-    private final IntSet blockStateIds = new IntOpenHashSet();
+    private final IntSet blockStateIds;
     // lists preferable for indexed iteration
-    private final List<Block> blockList = new ArrayList<>();
-    private final IntList blockStateIdList = new IntArrayList();
+    @Getter(lazy = true) private final List<Block> blockList = buildBlockList();
+    @Getter(lazy = true) private final IntList blockStateIdList = buildBlockStateIdList();
 
     public BlockOptionalMetaLookup(Set<Block> blocks) {
         this.blockSet = blocks;
+        this.blockStateIds = new IntOpenHashSet();
         for (Block block : blocks) {
             for (int stateId = block.minStateId(); stateId <= block.maxStateId(); stateId++) {
                 blockStateIds.add(stateId);
-                blockStateIdList.add(stateId);
             }
-            this.blockList.add(block);
         }
     }
+
     public BlockOptionalMetaLookup(Block... blocks) {
         this(Sets.newHashSet(blocks));
+    }
+
+    public BlockOptionalMetaLookup(IntSet blockStateIds) {
+        this.blockSet = new HashSet<>();
+        this.blockStateIds = blockStateIds;
+        for (int stateId : blockStateIds) {
+            Block block = BlockRegistry.REGISTRY.get(stateId);
+            if (block != null) {
+                this.blockSet.add(block);
+            }
+        }
+    }
+
+    public BlockOptionalMetaLookup(int... blockStateIds) {
+        this(new IntOpenHashSet(blockStateIds));
     }
 
     public boolean has(Block block) {
@@ -46,5 +64,13 @@ public class BlockOptionalMetaLookup {
 
     public boolean has(int state) {
         return blockStateIds.contains(state);
+    }
+
+    private IntList buildBlockStateIdList() {
+        return new IntArrayList(blockStateIds);
+    }
+
+    private List<Block> buildBlockList() {
+        return new ArrayList<>(blockSet);
     }
 }
