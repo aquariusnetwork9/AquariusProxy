a1e15e7f637913b7a20e56eaf6c933f7b3e8f1bf
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2025-08-12 16:19:12 -0700

    `q refresh whileNotOn2b2t on/off`

--- src/main/java/com/zenith/util/config/Config.java
+++ src/main/java/com/zenith/util/config/Config.java
@@ -620,6 +620,7 @@ public final class Config {
         public String proxyIP = "localhost";
         public int queueStatusRefreshMinutes = 5; // how often to refresh queue lengths
         public boolean dynamicQueueEtaEquation = true;
+        public boolean queueStatusRefreshWhileNotOn2b2t = true;
         public boolean healthCheck = true;
         public long playerListsRefreshIntervalMins = 1440L; // one day as default
         public final Spectator spectator = new Spectator();
