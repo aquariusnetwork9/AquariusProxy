5753ee98f049aaa955735ab8c8a5b26f8570e218
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2025-08-11 21:02:53 -0700

    update deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     `java-library`
     id("org.graalvm.buildtools.native") version "0.11.0"
-    id("com.gradleup.shadow") version "9.0.0-rc3"
+    id("com.gradleup.shadow") version "9.0.1"
     id("io.freefair.lombok") version "8.14"
     `maven-publish`
 }
